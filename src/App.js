ed8701cf7e4e88e11657ea0443bbee2f486d64e8
akash <akashpanicker17@gmail.com>
2024-02-16 15:03:52 +0530

    Added .env file

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Recipe from './Recipe';
 import React, { useEffect, useState } from "react";
-// APP_ID and APP_KEY hidden from public for privacy reasons. Please create your own at their site.
 
+// Import environment variables
+const APP_ID = process.env.REACT_APP_ID;
+const APP_KEY = process.env.REACT_APP_KEY;
+
+console.log(APP_ID)
 const App = () => {
-
-
-
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState('');
+
   useEffect(() => {
     getRecipes();
   }, [query]);
+
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
 
     const data = await response.json();
     console.log(data);
@@ -30,10 +33,11 @@ const App = () => {
     e.preventDefault();
     setQuery(search);
   }
+
   return (
     <div className="App">
-    <br></br>
-    <h1 className='heading-top'>Welcome to the Recipe App</h1><br></br>
+      <br />
+      <h1 className='heading-top'>Welcome to the Recipe App</h1><br />
       <form onSubmit={getSearch} className="search-form">
         <input className='search-bar' type='text' value={search} onChange={updateSearch} />
         <button className='search-button' type="submit"> Search</button>
